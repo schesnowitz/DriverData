7e846f55a3a5269d30287db5176bfa7247527fba
schesnowitz <steve@chesnowitz.com>
2017-06-15 04:28:36 -0400

    simple get data

--- app/src/main/java/com/chesnowitz/driverdata/MainActivity.java
+++ app/src/main/java/com/chesnowitz/driverdata/MainActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
+/*
     final ParseObject driverData = new ParseObject("DriverStatus");
     driverData.put("userEmail", "email@email.com");
     driverData.put("status", "onDuty");
@@ -26,6 +28,23 @@ public class MainActivity extends AppCompatActivity {
           Log.i("Driver -->", driverData.toString());
         } else {
           Log.i("Driver -->", e.toString());
+        }
+      }
+    });
+*/
+
+    ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("DriverStatus");
+
+    // gets item by the id -- id is auto created
+    parseQuery.getInBackground("cTqg4JRCy1", new GetCallback<ParseObject>() {
+      @Override
+      public void done(ParseObject object, ParseException e) {
+        if (e == null && object != null) {
+          Log.i("Email Value --> ", object.getString("userEmail"));
+          Log.i("Status Value --> ", object.getString("status"));
+          Log.i("Time in status --> ", Double.toString(object.getDouble("timeInStatus")));
+        } else {
+
         }
       }
     });
